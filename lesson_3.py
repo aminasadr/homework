e1ddbf0e3e0b21123ee79a884d49b51c215f1ef3
aminasadr <93466976+aminasadr@users.noreply.github.com>
2021-11-21 19:34:31 +0300

    added correct lesson_3

--- lesson_3.py
+++ lesson_3.py
@@ -1,39 +1,39 @@
-#def divide(x, y):
-    #try:
-        #result = int(x) / int(y)
-    #except ZeroDivisionError as err_1:
-        #return err_1
-    #except ValueError as err_2:
-        #return err_2
-    #return result
-#print(divide(input("enter x: "), input("enter y: ")))
+def divide(x, y):
+    try:
+        result = int(x) / int(y)
+    except ZeroDivisionError as err_1:
+        return err_1
+    except ValueError as err_2:
+        return err_2
+    return result
+print(divide(input("enter x: "), input("enter y: ")))
 
 
 
-#def information(name, surname, birth_date, city, email, phone_number):
-    #print(f"surname - {surname}; name - {name}; email - {email}; phone_number - {phone_number}; birth_date - {birth_date}; city - {city}")
-#information(name = input("enter your name: "), surname = input("enter your surname: "), birth_date = input("enter your birth_date: "),
-            #city = input("enter your city: "), email = input("enter your email: "), phone_number = input("enter your phone_number: "))
+def information(name, surname, birth_date, city, email, phone_number):
+    print(f"surname - {surname}; name - {name}; email - {email}; phone_number - {phone_number}; birth_date - {birth_date}; city - {city}")
+information(name = input("enter your name: "), surname = input("enter your surname: "), birth_date = input("enter your birth_date: "),
+            city = input("enter your city: "), email = input("enter your email: "), phone_number = input("enter your phone_number: "))
 
 
 
-#def divide(x, y, z):
-    #sum = x + y + z
-    #little = min(x, y, z)
-    #result = sum - little
-    #return result
-#print(divide(int(input("enter x: ")), int(input("enter y: ")), int(input("enter z: "))))
+def divide(x, y, z):
+    sum = x + y + z
+    little = min(x, y, z)
+    result = sum - little
+    return result
+print(divide(int(input("enter x: ")), int(input("enter y: ")), int(input("enter z: "))))
 
 
-#def my_func(x, y):
-    #n = 1
-    #while n <= abs(y):
-        #x_res = pow(x, n)
-        #n += 1
-    #else:
-        #result = 1 / x_res
-    #return result
-#print(my_func(2, -4))
+def my_func(x, y):
+    n = 1
+    while n <= abs(y):
+        x_res = pow(x, n)
+        n += 1
+    else:
+        result = 1 / x_res
+    return result
+print(my_func(2, -4))
 
 
 def my_sum():
